ea428521216606f49a58ff4a3d34d25561ce1da5
Aman <amanmfp025@gmail.com>
2023-12-14 11:56:50 +0530

    bug fix

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -117,7 +117,7 @@ const CartItems = () => {
           </Link>
         </div>
         <div className="cartitems-promocode">
-          <p>If you have a promo code entered it here.</p>
+          <p>If you have a promo code enter it here.</p>
           <div className="cartitems-promobox">
             <input type="text" placeholder="promocode" />
             <button>Submit</button>
